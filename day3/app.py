28c610e1ac557798bda24597ee4118bfbd4cb896
franky-106 <frankychannel0@outlook.com>
2025-12-18 14:37:37 +0800

    分支代码

---
+++ day3/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+# 后端传递数据到模板
+@app.route('/user/<username>')
+def show_user(username):
+    return render_template('user.html',
+                          username=username,
+                          is_admin=True,
+                          items=['苹果', '香蕉', '橙子'])
+
+@app.template_filter('datetimeformat')
+def datetimeformat(value, format='%Y-%m-%d %H:%M:%S'):
+    """自定义日期格式化过滤器"""
+    if value is None:
+        return ""
+    return value.strftime(format)
+
+@app.template_filter('phone_format')
+def phone_format(value):
+    """电话号码格式化"""
+    if not value:
+        return ""
+    return f"{value[:3]}-{value[3:7]}-{value[7:]} "
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
